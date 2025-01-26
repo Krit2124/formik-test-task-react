27d7c2a09790e7e019d052d15a14bbd6129a5e81
Krit2124 <kritserg2124@gmail.com>
2025-01-27 01:13:39 +0300

    fix: register error part 2

---
+++ src/shared/ui/FormikRadio/FormikRadio.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent, FC } from "react";
+import { Radio, RadioGroup } from "../index";
+import {withFormikField} from "@/shared/hocs";
+
+type FormikRadioProps = {
+  label: string;
+  name: string;
+  options: string[];
+  value?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const FormikRadioComponent: FC<FormikRadioProps> = ({ label, options, value, onChange, ...props }) => (
+  <div>
+    <label>{label}</label>
+    <RadioGroup {...props} value={value} onChange={onChange} className="flex gap-3">
+      {options.map((option) => (
+        <Radio key={option} value={option} className="cursor-pointer">
+          {option}
+        </Radio>
+      ))}
+    </RadioGroup>
+  </div>
+);
+
+const FormikRadio = withFormikField<FormikRadioProps>(FormikRadioComponent);
+
+export default FormikRadio;
+ 
